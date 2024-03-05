957188e2da71571c06a918e5f9b619b0d518986d
HP <nurullofza004@icloud.com>
2024-03-05 13:06:19 +0500

    updated texts

---
+++ lib/utils/strings.dart
@@ -0,0 +1,41 @@
+
+
+class Strings {
+  /// intro page texts
+  static String intro1 = 'Быстро доставим ваш товар';
+  static String intro2 = 'Доставим бесплатно первые 3 заказа';
+  static String intro3 = 'Быстрая и удобная оплата заказов';
+  static String buttonText = 'Продолжить';
+  /// auth page texts
+  static String authText1 = 'Введите номер телефона';
+  static String authText2 = 'Отправим смс с кодом подтверждения';
+  static String authText3 = 'При регистрации вы соглашаетесь с условиями использования и политикой конфиденциальности';
+  static String authText4 = 'Введите код';
+  static String authText5 = 'Отправили код на номер +998 90 654 16 05';
+  static String authText6 = 'Изменить номер';
+  /// auth error texts
+  static String errorText1 = 'Неверный код';
+  static String errorText2 = 'Отправить еще раз';
+  static String errorText3 = 'Время истекло';
+  /// auth registration texts
+  static String registerText1 = 'Регистрация';
+  static String registerText2 = 'ФИО';
+  static String registerText3 = 'Дата рождения';
+  static String hintText2 = 'Введите  дату рождения';
+  /// main page text
+  static String textfieldHintText = 'Поиск товаров и категорий';
+  /// ListView horizontal texts
+  static String category = 'Категории';
+  static String text1 = 'Распродажа';
+  static String text2 = 'Автотовары';
+  static String text3 = 'Бытовая техника';
+  static String text4 = 'Распродажа';
+  static String text5 = 'Кешбек 5%';
+  /// gridview text
+  static String recommend = 'Рекоммендуемое';
+  /// favourite page text
+  static String favouritePageText = 'Мои желания';
+  /// profile page text
+  static String profilePageText = 'Мои желания';
+
+}
