cc802add9f111e6bc29cc6351fc9bbd0df3fa492
HP <nurullofza004@icloud.com>
2024-03-06 02:22:19 +0500

    added strings

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -11,7 +11,7 @@ class Strings {
   static String authText2 = 'Отправим смс с кодом подтверждения';
   static String authText3 = 'При регистрации вы соглашаетесь с условиями использования и политикой конфиденциальности';
   static String authText4 = 'Введите код';
-  static String authText5 = 'Отправили код на номер +998 90 654 16 05';
+  static String authText5 = 'Отправили код на номер ';// here registered number should come
   static String authText6 = 'Изменить номер';
   /// auth error texts
   static String errorText1 = 'Неверный код';
@@ -38,4 +38,54 @@ class Strings {
   /// profile page text
   static String profilePageText = 'Мои желания';
 
+  /// catalog page texts
+  static String catalogText1 = 'Вы недавно искали';
+  static String catalogText2 = 'очистить';
+  static String catalogText3 = 'Популярные';
+  static String catalogText4 = 'Ничего не найдено из категории';
+  static String catalogText5 = 'Бытовая техника';
+  static String catalogText6 = 'Электроника';
+  static String catalogText7 = 'Авто запчасти';
+  static String catalogText8 = 'Еда';
+  static String catalogText9 = 'Спорт';
+  static String catalogText10 = 'Строительные материалы';
+  static String catalogText11 = 'Игрушки';
+  static String catalogText12 = 'Обувь';
+  static String catalogText13 = 'Одежда';
+  /// cart page texts
+  static String cartPageAppBarText = 'Корзина';
+  static String cartPageText1 = 'Выбрать';
+  static String cartPageText2 = 'Ваш заказ:';
+  static String cartPageText3 = 'Итого:*:';
+  static String cartPageText4 = 'Похожие товары';
+  static String cartPageText5 = '*Окончательная стоимость будет рассчитана после выбора способа доставки при оформлении заказа';
+  static String cartPageText6 = 'Перенести в избранные';
+  static String cartPageText7 = 'Удалить';
+  static String cartPageText8 = 'Оформление заказа';
+  static String cartPageText9 = 'Город получения';
+  static String cartPageText10 = 'Пункт выдачи UCommerce';
+  static String cartPageText11 = 'Можно забрать Завтра,бесплатно ';
+  static String cartPageText12 = 'Доставка за 1 день';
+  static String cartPageText13 = 'Срок хранения заказа - 5 дней';
+  static String cartPageText14 = 'Можно проверить и примерить товар';
+  static String cartPageText15 = 'Возврат товара быстро и без проблем';
+  static String cartPageText16 = 'Курьером до двери';
+  static String cartPageText17 = 'Доставим Завтра,бесплатно ';
+  static String cartPageText18 = 'Адрес доставки';
+  static String cartPageText19 = 'Изменить';
+  static String cartPageText20 = 'Получатель заказа';
+  static String cartPageText21 = 'Мы пришлем уведомление о статусе заказа на указанный вами телефон';
+  static String cartPageText22 = 'Способ оплаты';
+  static String cartPageText23 = 'Наличными или картой при получении';
+  static String cartPageText24 = 'Размещеная заказ,вы соглащаетесь на обработку персональных данных в соответсвии с Условиями, а так  же соглашаетесь с правилами  акции';
+  static String cartPageText25 = 'Итого';
+  static String cartPageText26 = 'UZCARD,Humo, Visa, MasterCard';
+  static String cartPageText27 = 'Выберите город получения';
+  static String cartPageHintText1 = 'Город*';
+  static String cartPageHintText2 = 'Фамилия *';
+  static String cartPageHintText3 = 'Имя*';
+  static String cartPageHintText4 = 'Номер телефона*';
+  static String cartPageHintTex5 = 'Промокод';
+  static String cartPageHintTex6 = 'Найти город';
+  static String cartPageButtonText = 'Оформить заказ';
 }
