4596d3e5792c22258ae905f8d4d020802a6c5742
HP <nurullofza004@icloud.com>
2024-03-10 16:15:03 +0500

    made some changes

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -62,13 +62,14 @@ class _ProfilePageState extends State<ProfilePage> {
             SizedBox(height: screenHeight / 30,),
             Container(
               margin: EdgeInsets.only(right: screenWidth / 50,left: screenWidth / 50),
-              height: screenHeight / 3.8,
+              height: screenHeight / 3.2,
               width: screenHeight / 2.0,
               decoration: BoxDecoration(
                 color: white,
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ListTile(
                     leading:  Icon(Icons.shopping_cart_outlined,color:blue,),
@@ -93,13 +94,14 @@ class _ProfilePageState extends State<ProfilePage> {
             SizedBox(height: screenHeight / 50,),
             Container(
               margin: EdgeInsets.only(right: screenWidth / 50,left: screenWidth / 50),
-              height: screenHeight / 2.2,
+              height: screenHeight / 2,
               width: screenHeight / 2.0,
               decoration: BoxDecoration(
                 color: white,
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ListTile(
                     leading: Icon(Icons.percent,color: blue,),
@@ -145,18 +147,17 @@ class _ProfilePageState extends State<ProfilePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ListTile(
-                    leading: CircleAvatar(backgroundImage: AssetImage('assets/images/profile_image/Group (1).png')),
-                    title: Expanded(
-                      child: Row(
-                        children: [
-                          Text(Strings.profilePageText11,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
-                          Spacer(),
-                          Text(Strings.profilePageText16,style: TextStyle(fontSize: screenHeight / 40,color: grey,fontWeight: FontWeight.w500),),
-                        ],
-                      ),
-                    ),
-                    trailing: Icon(Icons.navigate_next,color: black,),
+                  Row(
+                    children: [
+                      SizedBox(width: screenWidth / 30,),
+                      CircleAvatar(backgroundImage: AssetImage('assets/images/profile_image/Group (1).png')),
+                      SizedBox(width: screenWidth / 20,),
+                      Text(Strings.profilePageText11,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
+                      Spacer(),
+                      Text(Strings.profilePageText16,style: TextStyle(fontSize: screenHeight / 40,color: grey,fontWeight: FontWeight.w500),),
+                      Icon(Icons.navigate_next,color: black,),
+                      SizedBox(width: screenWidth / 18,),
+                    ],
                   ),
                 ],
               )
@@ -164,31 +165,33 @@ class _ProfilePageState extends State<ProfilePage> {
             SizedBox(height: screenHeight / 50,),
             Container(
               margin: EdgeInsets.only(right: screenWidth / 50,left: screenWidth / 50),
-              height: screenHeight / 5.6,
+              height: screenHeight / 5,
               width: screenHeight / 2.0,
               decoration: BoxDecoration(
                 color: white,
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ListTile(
-                    leading: Icon(Icons.location_on_outlined,color: blue,),
-                    title: Expanded(
-                      child: Row(
-                        children: [
-                          Text(Strings.profilePageText12,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
-                          Spacer(),
-                          Text(Strings.profilePageText17,style: TextStyle(fontSize: screenHeight / 40,color: grey,fontWeight: FontWeight.w500),),
-                        ],
-                      ),
-                    ),
-                    trailing: Icon(Icons.navigate_next,color: black,),
+                  SizedBox(height: screenHeight / 50,),
+                  Row(
+                    children: [
+                      SizedBox(width: screenWidth / 30,),
+                      Icon(Icons.location_on_outlined,color: blue,),
+                      SizedBox(width: screenWidth / 20,),
+                      Text(Strings.profilePageText12,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
+                      Spacer(),
+                      Text(Strings.profilePageText17,style: TextStyle(fontSize: screenHeight / 40,color: grey,fontWeight: FontWeight.w500),),
+                      Icon(Icons.navigate_next,color: black,),
+                      SizedBox(width: screenWidth / 18,),
+                    ],
                   ),
+                  SizedBox(height: screenHeight / 50,),
                   Divider(indent: screenWidth / 8,color: greyShade2,),
                   ListTile(
                     leading: Icon(Icons.map_outlined,color: blue,),
-                    title: Text(Strings.profilePageText13,style: TextStyle(fontSize: screenHeight / 40),),
+                    title: Text(Strings.profilePageText13,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
                     trailing: Icon(Icons.navigate_next,color: black,),
                   ),
                 ],
@@ -197,22 +200,23 @@ class _ProfilePageState extends State<ProfilePage> {
             SizedBox(height: screenHeight / 50,),
             Container(
               margin: EdgeInsets.only(right: screenWidth / 50,left: screenWidth / 50),
-              height: screenHeight / 5.6,
+              height: screenHeight / 5,
               width: screenHeight / 2.0,
               decoration: BoxDecoration(
                 color: white,
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ListTile(
-                    leading: Icon(Icons.location_on_outlined,color: blue,),
+                    leading: Icon(Icons.help_outline_outlined,color: blue,),
                     title: Text(Strings.profilePageText14,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
                     trailing: Icon(Icons.navigate_next,color: black,),
                   ),
                   Divider(indent: screenWidth / 8,color: greyShade2,),
                   ListTile(
-                    leading: Icon(Icons.map_outlined,color: blue,),
+                    leading: Icon(Icons.email_outlined,color: blue,),
                     title: Text(Strings.profilePageText15,style: TextStyle(fontSize: screenHeight / 40,fontWeight: FontWeight.w500),),
                     trailing: Icon(Icons.navigate_next,color: black,),
                   ),
