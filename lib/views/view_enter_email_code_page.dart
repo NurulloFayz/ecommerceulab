d6ef5c88cb13cdb32744d444bfefa67714f864a0
HP <nurullofza004@icloud.com>
2024-03-25 11:30:21 +0500

    new github

--- lib/views/view_enter_email_code_page.dart
+++ lib/views/view_enter_email_code_page.dart
@@ -6,7 +6,6 @@ import 'package:ecommerce_ulab/pages/auth_pages/enter_name_bdate.dart';
 import 'package:ecommerce_ulab/pages/pages.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -28,10 +27,10 @@ class ViewEnterEmailCodePage extends ChangeNotifier{
     Navigator.pushReplacementNamed(context, EmailPage.id);
   }
   void navigateToEnterNameBDatePage(BuildContext context) {
-    Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => EnterNameBDatePage()));
+    Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => const EnterNameBDatePage()));
   }
   void navigateToMyPages(BuildContext context) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyPages()));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyPages()));
   }
   Future<void> enterPassword (String password,String email,BuildContext context) async {
     var url = Uri.parse('https://ulab-market-backend.onrender.com/auth/verifyCode');
@@ -52,11 +51,10 @@ class ViewEnterEmailCodePage extends ChangeNotifier{
     if (response.statusCode == 200) {
       SharedPreferences preferences = await SharedPreferences.getInstance();
       preferences.setString('email', checkEmail);
-      //view.navigateToMyPages(context);
-
       // Successful login, handle response data
       var responseData = jsonDecode(response.body);
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyPages()));
+      navigateToMyPages(context);
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyPages()));
       print('Login successful: $responseData');
     } else {
       // Handle login failure
