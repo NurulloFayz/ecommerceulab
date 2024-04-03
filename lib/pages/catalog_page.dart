3b974aefb36e5545b1f0eeff47e99aa96c5ec53a
HP <nurullofza004@icloud.com>
2024-04-03 12:22:03 +0500

    added category page

--- lib/pages/catalog_page.dart
+++ lib/pages/catalog_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../model/category_model.dart';
+import '../utils/color.dart';
 import '../utils/strings.dart';
 
 class CatalogPage extends StatefulWidget {
@@ -28,7 +29,108 @@ class _CatalogPageState extends State<CatalogPage> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body:
+      body:Column(
+        children: [
+          SizedBox(height: screenHeight / 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // IconButton(
+              //   icon: Icon(
+              //     CupertinoIcons.left_chevron,
+              //     color: black,
+              //     size: screenHeight / 40,
+              //   ),
+              //   onPressed: () {
+              //     view.navigateToMyPages(context);
+              //   },
+              // ),
+              Container(
+                width: screenWidth / 1.1,
+                child: TextField(
+                  style: TextStyle(fontSize: screenHeight / 40),
+                  controller: view.searchController,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                      contentPadding: EdgeInsets.all(
+                          MediaQuery.of(context).size.height / 60),
+                      filled: true,
+                      fillColor:view.color,
+                      hintText: Strings.homePageHintText,
+                      hintStyle: TextStyle(
+                          fontSize:
+                          MediaQuery.of(context).size.height / 45,
+                          color: Colors.grey.withOpacity(0.8)),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(8))),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: screenHeight * .01,
+          ),
+          Divider(
+            color: greyShade1,
+            height: screenHeight * .01,
+            thickness: screenHeight * .005,
+          ),
+          Expanded(
+            child: FutureBuilder<List<CategoryModel>>(
+                future: lists,
+                builder: (context,snapshot) {
+                  if(snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator(),);
+                  } else if(snapshot.hasError) {
+                    return Center(child: Text('error${snapshot.error}'),);
+                  } else {
+                    final categoryList = snapshot.data;
+                    if(categoryList!.isEmpty) {
+                      return Text('no recipes found');
+                    }
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: categoryList.length,
+                        itemBuilder: (context,index) {
+                          final itemList = categoryList[index];
+                          return Column(
+                            children: [
+                              SizedBox(height: screenHeight / 100,),
+                              ListTile(
+                                leading: Container(
+                                  width: screenWidth / 6,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: Colors.grey.withOpacity(0.4),
+                                    image: DecorationImage(
+                                      image: Image.network(itemList.image,fit: BoxFit.cover,height: screenHeight / 10,).image
+                                    )
+                                  ),
+                                ),
+                                title: Text(itemList.name,style: TextStyle(fontSize: screenHeight / 45),),
+                                trailing: const Icon(Icons.navigate_next,color: Colors.grey,),
+                              ),
+                              SizedBox(height: screenHeight / 100,),
+                              Divider(
+                                thickness: 1,
+                                indent: screenWidth / 20,
+                                color: Colors.grey.withOpacity(0.3),
+                              ),
+                            ],
+                          );
+                        }
+                    );
+                  }
+                }
+            ),
+          ),
+        ],
+      ),
       // SingleChildScrollView(
       //   child: Container(
       //     width: width,
@@ -186,77 +288,88 @@ class _CatalogPageState extends State<CatalogPage> {
       //     ),
       //   ),
       // ),
-      SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: screenHeight / 20,),
-            Container(
-              height: screenHeight,
-              margin: EdgeInsets.only(right: screenWidth / 40,left: screenWidth / 40),
-              child: TextField(style: TextStyle(
-                  fontSize: screenHeight / 40),
-                controller: view.searchController,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    contentPadding: EdgeInsets.all(
-                        MediaQuery.of(context).size.height / 60),
-                    filled: true,
-                    fillColor:view.color,
-                    hintText: Strings.homePageHintText,
-                    hintStyle: TextStyle(
-                        fontSize: MediaQuery.of(context).size.height / 45,
-                        color: Colors.grey.withOpacity(0.8)),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(8))),
-              ),
-            ),
-            SizedBox(height: screenHeight / 40,),
-            Container(
-              height: screenHeight / 280,
-              width: screenWidth,
-              color: Colors.grey.withOpacity(0.1),
-            ),
-            SizedBox(height: screenHeight / 40,),
-            Expanded(
-              child: Container(
-                height: screenHeight / 1,
-                child: FutureBuilder<List<CategoryModel>>(
-                    future: lists,
-                    builder: (context,snapshot) {
-                      if(snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator(),);
-                      } else if(snapshot.hasError) {
-                        return Center(child: Text('error${snapshot.error}'),);
-                      } else {
-                        final categoryList = snapshot.data;
-                        if(categoryList!.isEmpty) {
-                          return Text('no recipes found');
-                        }
-                        return ListView.builder(
-                          shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: categoryList.length,
-                            itemBuilder: (context,index) {
-                              final itemList = categoryList[index];
-                              return ListTile(
-                                leading: Icon(Icons.image) ,
-                                title: Text(itemList.name,style: TextStyle(fontSize: screenHeight / 50),),
-                                trailing: const Icon(Icons.navigate_next),
-                              );
-                            }
-                        );
-                      }
-                    }
-                ),
-              ),
-            )
-          ],
-        ),
-      )
+      // SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       SizedBox(height: screenHeight / 20,),
+      //       Container(
+      //         height: screenHeight,
+      //         margin: EdgeInsets.only(right: screenWidth / 40,left: screenWidth / 40),
+      //         child: TextField(style: TextStyle(
+      //             fontSize: screenHeight / 40),
+      //           controller: view.searchController,
+      //           decoration: InputDecoration(
+      //               prefixIcon: Icon(
+      //                 Icons.search,
+      //                 color: Colors.grey,
+      //               ),
+      //               contentPadding: EdgeInsets.all(
+      //                   MediaQuery.of(context).size.height / 60),
+      //               filled: true,
+      //               fillColor:view.color,
+      //               hintText: Strings.homePageHintText,
+      //               hintStyle: TextStyle(
+      //                   fontSize: MediaQuery.of(context).size.height / 45,
+      //                   color: Colors.grey.withOpacity(0.8)),
+      //               border: OutlineInputBorder(
+      //                   borderSide: BorderSide.none,
+      //                   borderRadius: BorderRadius.circular(8))),
+      //         ),
+      //       ),
+      //       SizedBox(height: screenHeight / 40,),
+      //       Container(
+      //         height: screenHeight / 280,
+      //         width: screenWidth,
+      //         color: Colors.grey.withOpacity(0.1),
+      //       ),
+      //       SizedBox(height: screenHeight / 40,),
+      //       Expanded(
+      //         child: Container(
+      //           height: screenHeight / 1,
+      //           child: FutureBuilder<List<CategoryModel>>(
+      //               future: lists,
+      //               builder: (context,snapshot) {
+      //                 if(snapshot.connectionState == ConnectionState.waiting) {
+      //                   return const Center(child: CircularProgressIndicator(),);
+      //                 } else if(snapshot.hasError) {
+      //                   return Center(child: Text('error${snapshot.error}'),);
+      //                 } else {
+      //                   final categoryList = snapshot.data;
+      //                   if(categoryList!.isEmpty) {
+      //                     return Text('no recipes found');
+      //                   }
+      //                   return ListView.builder(
+      //                     shrinkWrap: true,
+      //                       physics: const NeverScrollableScrollPhysics(),
+      //                       itemCount: categoryList.length,
+      //                       itemBuilder: (context,index) {
+      //                         final itemList = categoryList[index];
+      //                         return ListTile(
+      //                           leading: Container(
+      //                             height: screenHeight / 20,
+      //                             width: screenWidth / 8,
+      //                             decoration: BoxDecoration(
+      //                               borderRadius: BorderRadius.circular(8),
+      //                               color: Colors.grey.withOpacity(0.4),
+      //                               image: DecorationImage(
+      //                                 image: AssetImage(itemList.image,),
+      //                                 fit: BoxFit.cover
+      //                               )
+      //                             ),
+      //                           ),
+      //                           title: Text(itemList.name,style: TextStyle(fontSize: screenHeight / 50),),
+      //                           trailing: const Icon(Icons.navigate_next),
+      //                         );
+      //                       }
+      //                   );
+      //                 }
+      //               }
+      //           ),
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // )
     );
   }
 }
